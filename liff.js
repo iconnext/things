c19557bf63ce228f0a06d220f05f213f27ec416c
iconnext <watchara.sukka@iconnext.net>
2019-07-18 14:34:42 +0700

    update user service id

--- liff.js
+++ liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '6557521130802833076'; // LED, Button
+const USER_SERVICE_UUID         = '299413d3-21d7-4f49-9552-9e4222dfbed9'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
